2a1b44006206044d351e1373fea2aea6f4a3d989
Rene Cunningham <rene@compounddata.com>
2020-09-05 21:40:31 +1000

    test for tz

---
+++ internal/tz/tz_test.go
@@ -0,0 +1,45 @@
+package tz
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func tempSymlink(dest string) (string, error) {
+	file, err := ioutil.TempFile("", "wtz")
+	if err != nil {
+		return "", err
+	}
+	os.Remove(file.Name())
+	if err = os.Symlink(dest, file.Name()); err != nil {
+		return "", err
+	}
+	return file.Name(), nil
+}
+
+func TestZoneinfo(t *testing.T) {
+	for _, tc := range []struct {
+		zoneinfo string
+		want     string
+	}{
+		{"/usr/share/zoneinfo/Australia/Melbourne", "Australia/Melbourne"},
+		{"../../../../../../../../..//usr/share/zoneinfo/Australia/ACT", "Australia/ACT"},
+		{"/usr/share/zoneinfo/NZ", "NZ"},
+	} {
+		s, err := tempSymlink(tc.zoneinfo)
+		if err != nil {
+			t.Error(err)
+		}
+		defer os.Remove(s)
+
+		_t := &tz{s}
+		got, err := _t.Zoneinfo()
+		if err != nil {
+			t.Error(err)
+		}
+		if got != tc.want {
+			t.Errorf("want %s but got %s", tc.want, got)
+		}
+	}
+}
